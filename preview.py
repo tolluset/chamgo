05bfc86fd751046f3c0925628bf2009537539552
tolluset <dlqud19@gmail.com>
2024-11-21 08:05:02 +0900

    refactor: check settings

--- preview.py
+++ preview.py
@@ -1,21 +1,38 @@
+import os
 import subprocess
+import logging
 import dotenv
 
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
+logging.basicConfig(level=logging.INFO)
 dotenv.load_dotenv()
+
+
+LLM_MODEL = os.getenv("LLM_MODEL") or "o1-mini"
+LANGUAGE = os.getenv("LANGUAGE") or "ENGLISH"
 
 llm = ChatOpenAI(model="o1-mini")
 
 
 def main():
+    check_env()
+
     diff = get_diff()
 
     review = get_review(diff)
 
     formatted_review = format_review(review)
 
-    print(formatted_review)
+    logging.info(formatted_review)
+
+
+def check_env():
+    required_vars = ["OPENAI_API_KEY"]
+
+    for var in required_vars:
+        if not os.getenv(var):
+            raise EnvironmentError(f"Required environment variable '{var}' is not set.")
 
 
 def get_diff() -> str:
@@ -27,12 +44,12 @@ def get_diff() -> str:
     return result.stdout.decode("utf-8")
 
 
-def get_review(diff: str):
+def get_review(diff: str) -> str:
     review = llm.invoke(
         [
             (
                 "assistant",
-                "Review user git diff. Check typo, grammar, and code style.",
+                "Review the user's git diff, checking for typos, grammar, and code style issue.",
             ),
             ("human", diff),
         ]
@@ -41,7 +58,7 @@ def get_review(diff: str):
     return review
 
 
-def format_review(review: str):
+def format_review(review: str) -> str:
     formatted_review = review
 
     return formatted_review
